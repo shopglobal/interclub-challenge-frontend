fcf504e7ce0710e57fefa2df18fcb76daed8ca8e
yannunes <yannunes@gmail.com>
2017-11-25 00:52:04 -0300

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-a
+## Interclub Challenge - Frontend
+
+To get started:
+1. yarn install
+2. yarn run start
---
+++ src/MemberList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+import MemberItem from './MemberItem';
+
+const StyledWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    padding: 50px calc((100vw - 860px) / 2);
+    justify-content: space-between;
+`
+
+export default class MemberList extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            members: []
+        }
+    }
+
+    async componentWillMount() {
+        try {
+            const res = await axios.get('http://localhost:4000/api/list-members');
+            const members = res.data;
+            this.setState({members});
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    render() {
+        const mappedMembers =  this.state.members.map(member => <MemberItem member={member} key={member.id} />);
+
+        return (
+            <StyledWrapper>
+                {mappedMembers}
+            </StyledWrapper>
+        )
+    }
+}
