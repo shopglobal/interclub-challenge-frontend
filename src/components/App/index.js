aa5ba5114efc8055750cb635701097cdee120cd8
yannunes <yannunes@gmail.com>
2017-11-25 21:53:20 -0300

    Logo is now home button

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
 import MemberList from '../../containers/MembersList/index';
 import Details from '../../containers/Details/index';
@@ -10,9 +10,11 @@ export default class App extends Component {
   render () {
     return (
       <StyledWrapper>
-        <StyledLogoLink href='https://interclub.io' target='_blank'>
-          <img src='/assets/inv_logo_48x48.png' />
-        </StyledLogoLink>
+        <Link to="/">
+          <StyledLogoLink>
+            <img src='/assets/inv_logo_48x48.png' />
+          </StyledLogoLink>
+        </Link>
         <Switch>
           <Route exact path="/" component={MemberList}/>
           <Route exact path="/details/:id" component={Details}/>
